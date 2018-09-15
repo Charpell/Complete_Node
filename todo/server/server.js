b92394b7bbc4c04d978366f7c5571163b9392b95
Charpell <charpellumeh@gmail.com>
2018-09-15 03:07:30 +0100

    Create /POST todo route

--- todo/server/server.js
+++ todo/server/server.js
@@ -14,6 +14,17 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 
 app.listen(port, () => {
